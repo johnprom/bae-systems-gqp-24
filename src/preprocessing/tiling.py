6e5650d9a22d6bc379dade79d2d42690a60d6a31
Dan Fox <dfox@alum.mit.edu>
2024-10-18 06:01:06 -0400

    Take out implementation integration comments

--- src/preprocessing/tiling.py
+++ src/preprocessing/tiling.py
@@ -10,17 +10,6 @@ def tiling(ctxt):
     
     config = ctxt.get_pipeline_config() # dictionary of yaml configuration, in case you need it
     
-    # TODO: AMIT: Remove this code block entirely
-    # Dummy code for testing - for now just copies images and text files, does no processing on them
-    # image_filelist = [x for x in os.listdir(ctxt.input_images_dir) if x.endswith('tif')]
-    # label_filelist = [x for x in os.listdir(ctxt.input_images_dir) if x.endswith('txt')]
-    # for filename in image_filelist:
-    #     shutil.copy2(os.path.join(ctxt.input_images_dir, filename), ctxt.interim_images_dir)
-    # for filename in label_filelist:
-    #     shutil.copy2(os.path.join(ctxt.input_images_dir, filename), ctxt.interim_images_dir)
-    # TODO: AMIT: End of code block to remove
-
-    # Note: The following is Amit's code. It has been tested on the "happy path." He gets the credit for it.
     tile_size = config['preprocess_methods']['tiling']['image_size']
     overlap = config['preprocess_methods']['tiling']['stride']
     tiler = XViewTiler(tile_size=tile_size, overlap=overlap)
@@ -32,9 +21,6 @@ def tiling(ctxt):
     
     result_dict = tiler.get_class_wise_data(features, ctxt.input_images_dir, ctxt.target_labels, ctxt.train_labels)
     
-    # print(result_dict)
-    # TODO: AMIT: The following code puts the .txt files in the same directory as the .png files (ctxt.interim_images_dir)
-    # Is this okay? If not, add a configuration entry to pipeline_config.yaml file. Coordinate with DAN.
     tiler.tile_image_and_save(result_dict, ctxt.input_images_dir, ctxt.interim_images_dir, ctxt.interim_images_dir)
 
     print("Finished running tiling approach.")
